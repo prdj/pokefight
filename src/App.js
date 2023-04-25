347979ce642bedb4e62181a1c525a34f6ab2fdd6
prdj <pedrojr_dantas@hotmail.com>
2023-04-25 18:19:30 +0200

    implemented axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import PokemonDisplay from './components/pokemonDisplay';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [pokemons, getPokemons] = useState('');
+  
+  const url = 'http://localhost:8008/pokemon';
+
+  useEffect(() => {
+    getAllPokemons();
+  }, [])
+
+  const getAllPokemons = () => {
+    axios.get(`${url}`)
+    .then((response) => {
+      const allPokemons = response.data;
+      // add our data to state
+      getPokemons(allPokemons)
+    })
+    .catch(error => console.error(`Error: ${error}`))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PokemonDisplay pokemons={pokemons}/>
   );
 }
 
---
+++ src/components/pokemonDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PokemonDisplay (props) {
+
+    const {pokemons} = props;
+    
+    console.log(pokemons)
+
+    return (
+        <div>Pokemon app</div>
+    )
+}
